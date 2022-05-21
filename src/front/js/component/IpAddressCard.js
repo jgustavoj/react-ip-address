af0dbd954d6f6e8a6807f98c5e1457a0011f7f03
jgustavoj <gjime011@fiu.edu>
2022-05-21 14:19:51 -0400

    mobile view z-index complete with some data

--- src/front/js/component/IpAddressCard.js
+++ src/front/js/component/IpAddressCard.js
@@ -1,17 +1,25 @@
 import React from "react";
 
-export const IpAddressCard = () => {
+export const IpAddressCard = (props) => {
   return (
     <>
-      <div className="card">
-        <p>IP Address</p>
-        <br />
-        <p>Location</p>
-        <br />
-        <p>Timezone</p>
-        <br />
-        <p>ISP</p>
-        <br />
+      <div className="info-card d-flex align-items-center d-flex flex-column py-3">
+        <div className="info-card__value d-flex align-items-center d-flex flex-column">
+          <p>IP Address</p>
+          {props.ip}
+        </div>
+        <div className="info-card__value d-flex align-items-center d-flex flex-column">
+          <p>Location</p>
+          {props.city}, {props.region} {props.postalCode}
+        </div>
+        <div className="info-card__value d-flex align-items-center d-flex flex-column">
+          <p>Timezone</p>
+          {props.timezone}
+        </div>
+        <div className="info-card__value d-flex align-items-center d-flex flex-column">
+          <p>ISP</p>
+          {props.isp}
+        </div>
       </div>
     </>
   );
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -14,14 +14,24 @@ export const Home = () => {
       <div className="container-fluid">
         <div className="jumbo">
           <h1 className="display-5 px-3">IP Address Tracker</h1>
-          <InputField style={{ paddingTop: "1.5rem" }} />
+          <InputField
+            style={{
+              paddingTop: "1.5rem",
+              paddingLeft: "48px",
+              paddingRight: "42px",
+            }}
+          />
         </div>
-        <IpAddressCard />
+        <IpAddressCard
+          ip={store.content.ip}
+          city={store.content.location.city}
+          region={store.content.location.region}
+          postalCode={store.content.location.postalCode}
+          timezone={store.content.location.timezone}
+          isp={store.content.isp}
+        />
         <LeafMap />
       </div>
     </>
   );
 };
-
-// {/* <div className="ip-address-card">
-// </div> */}
